21240ef977781a717d48412e84b681188841eaf3
MrDog210 <tusakf@gmail.com>
2025-01-10 20:21:59 +0100

    Update Program.cs

--- InutilisChain/Program.cs
+++ InutilisChain/Program.cs
@@ -9,17 +9,21 @@ if (args.Contains("-server"))
 {
     var blockchainServer = new BlockchainServer();
     var mqttBlockchainServer = new MqttBlockchainServer(blockchainServer);
+    RESTServer restServer = new RESTServer(blockchainServer);
     mqttBlockchainServer.StartServer();
     Console.WriteLine("Started mqtt server");
     var serverThread = new Thread(() => blockchainServer.StartServer());
     serverThread.Start();
     Console.WriteLine("Started blockchain server");
     blockchainServer.StartMining();
+    var restThread = new Thread(() => restServer.Start());
+    restThread.Start();
     Console.WriteLine("Started mining");
     Console.WriteLine("Press any key to stop the server");
     Console.ReadLine();
 
     serverThread.Abort();
+    restThread.Abort();
 }
 else
 {
